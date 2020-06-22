643a607306cff4149c6783b2aa08c2a0db5ec84f
Catherine Straus <vnair611@gmail.com>
2020-06-22 05:16:26 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -32,7 +32,7 @@ function removeElementFromBeginningOfArray(array){
 }
 
 function destructivelyRemoveElementFromEndOfArray(array){
-  array = array.pop()
+  array.pop()
 }
 
 function removeElementFromEndOfArray(array){
