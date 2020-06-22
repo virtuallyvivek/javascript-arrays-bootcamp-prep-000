b63b24eeaa021f33dbc22adaa13272074b00632b
Catherine Straus <vnair611@gmail.com>
2020-06-22 03:52:27 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,19 @@
+var chocolateBars = ["snickers","hundred grand","kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element){
+  return [element,...array]
+}
+
+
+function destructivelyAddElementToBeginningOfArray(array ,element){
+  chocolateBars.unshift(element)
+}
+
+
+function addElementToEndOfArray(array, element){
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array ,element){
+  chocolateBars.unshift(element)
+}
