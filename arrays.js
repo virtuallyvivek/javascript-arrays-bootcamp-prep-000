ec03c5c543e2bd61cff4a0b34f9a6fa0eda86dbb
Catherine Straus <vnair611@gmail.com>
2020-06-22 05:11:18 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -36,3 +36,7 @@ function destructivelyRemoveElementFromEndOfArray(array){
   array = array.pop()
   return array
 }
+
+function removeElementFromEndOfArray(array){
+  return array.slice(0, array.length - 1)
+}
