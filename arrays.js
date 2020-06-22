f9ad8e028dad4a81432335cbe70e8bed630a9483
Catherine Straus <vnair611@gmail.com>
2020-06-22 05:06:43 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -31,3 +31,5 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 function removeElementFromBeginningOfArray(array){
   return array.slice(1)
 }
+
+destructivelyRemoveElementFromEndOfArray
