8bd55962b4c765c10be45a78808daeeaa5c73f72
Catherine Straus <vnair611@gmail.com>
2020-06-22 04:48:28 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,7 @@ function addElementToBeginningOfArray(array, element){
 
 
 function destructivelyAddElementToBeginningOfArray(array ,element){
-  array.unshift('element')
+  array.unshift(element)
 }
 
 
@@ -15,7 +15,7 @@ function addElementToEndOfArray(array, element){
 }
 
 function destructivelyAddElementToEndOfArray(array ,element){
-  array.unshift('element')
+  array.unshift(element)
 }
 
 
